8baa130e0c36d9d4dcee706d57db1dc2facda10f
matthewmsaucedo <themattsaucedo@gmail.com>
2019-09-19 23:55:24 -0400

    feat(*): bootstrap .Net Core api

---
+++ README.md
@@ -0,0 +1,5 @@
+# RSOhub
+
+### Technologies Used
+Angular 8, .NET Core 2, SQLite
+
---
+++ RSOhub.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace RSOhub.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
